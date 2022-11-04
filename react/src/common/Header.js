8a3577fe4d0b5b37bdf35cbffb46f66de59cd51c
dearnono2 <dearnono2@gmail.com>
2022-11-04 09:48:15 +0900

    react router 메뉴 활성화

--- react/src/common/Header.js
+++ react/src/common/Header.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 function Header() {
+  const activeStyle = { color: 'hotpink' };
   return (
-    <div>Header</div>
+    <header>
+      <h1>
+        <NavLink to='/'>LOGO</NavLink>
+      </h1>
+
+      <ul id="gnb">
+        <li>
+          <NavLink to='/list' style={({ isActive }) => (isActive ? activeStyle : null)}>Show List</NavLink>
+        </li>
+        <li>
+          <NavLink to='/create' style={({ isActive }) => (isActive ? activeStyle : null)}>Write Post</NavLink>
+        </li>
+      </ul>
+    </header>
   )
 }
 
