e085bdd0a1ac36fa08d6dd31e603e88e6d8cfba9
dearnono2 <dearnono2@gmail.com>
2022-11-07 10:18:32 +0900

    수정할 데이터 Edit컴포넌트의 폼에 출력

--- react/src/community/Edit.js
+++ react/src/community/Edit.js
@@ -1,18 +1,20 @@
-import Layout from "../common/Layout";
+import Layout from '../common/Layout';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from "axios";
+import axios from 'axios';
 
 function Edit() {
   const params = useParams();
-  const [Detail, setDetail] = useState(null);
+  const [Detail, setDetail] = useState({});
   const [Title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [Content, setContent] = useState('');
+
   useEffect(() => {
-    const item = {num: params.num}
+    const item = { num: params.num };
+
     axios.post('/api/community/detail', item)
       .then((res) => {
-        if(res.data.success) {
+        if (res.data.success) {
           console.log(res.data.detail);
           setDetail(res.data.detail);
         }
@@ -20,11 +22,20 @@ function Edit() {
       .catch(err => console.log(err));
   }, [])
 
+  useEffect(() => {
+    setTitle(Detail.title);
+    setContent(Detail.content);
+  }, [Detail])
+
   return (
     <Layout name={'Edit'}>
+      <label htmlFor="title">Title</label>
+      <input type="text" value={Title || ''} id='title' onChange={e => setTitle(e.target.value)} />
 
+      <label htmlFor="content">Content</label>
+      <textarea name="content" id="contend" cols="30" rows="4" value={Content || ''} onChange={e => setContent(e.target.value)}></textarea>
     </Layout>
-  )
+  );
 }
 
-export default Edit
+export default Edit;
